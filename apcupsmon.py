f44aeaa984975db2cc594ddc84f340578b217d3f
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-05-31 12:31:52 +0530

    apcupsmon: Added support for analysis of power from log file
    apcupsmon -a <file>
    
    apcupsmon --analyze <file>

--- apcupsmon.py
+++ apcupsmon.py
@@ -3,6 +3,7 @@ import os
 import time
 import sys
 from datetime import datetime
+from datetime import timedelta
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r",low=0,returnLine=True):
 # thanks to https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
     """
@@ -27,10 +28,111 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total: 
         print()
+def fileToCumPower(newpath):
+    #pass1 - get average
+    inputFileName=newpath
+    #inputFileName="apcupsmon_2652020.log.gz"
+    fileName=None
+    compress=False # Compress any way but can be changed to true to not preserve state...
+    if(inputFileName.endswith(".gz")):
+        #detected gz compression
+        compress=True
+        os.system("gunzip "+inputFileName)
+        fileName=inputFileName[:-3]
+    else:
+        fileName=inputFileName
+    entries=0
+    averageVolt=0
+    averageWatt=0
+    averageLoad=0
+    f=open(fileName)
+    f.readline()
+    while(True):
+        line=f.readline()
+        if(line=='\n' or line==""):
+            break
+        else:
+            time,volt,watt,load,transfers=line.strip().split("\t")
+            hrs,minutes,seconds=time.split(":")
+            averageLoad+=int(load)
+            averageVolt+=int(volt)
+            averageWatt+=int(watt)
+            entries+=1
+    f.close()
+    #print(averageLoad,averageVolt,averageWatt)
+    averageLoad/=entries
+    averageWatt/=entries
+    averageVolt/=entries
+    #print(averageLoad,averageVolt,averageWatt)
+
+
+    #pass2 - get wattage
+    cumWattsSeconds=0
+    cumTime=0
+    f=open(fileName)
+    f.readline()
+    prev=None
+    run=True
+    while(run):
+        time,volt,watt,load,transfers=[0,0,0,0,0]
+        hrs,minutes,seconds=[0,0,0]
+        while True:
+            line=f.readline()
+            if(line==""):
+                run=False
+                break
+            try:
+                time,volt,watt,load,transfers=line.strip().split("\t")
+                hrs,minutes,seconds=time.split(":")
+                break
+            except:
+                print("Error",line)
+        if(run==False):
+            break
+        else:
+            if(prev==None):
+                prev=[hrs,minutes,seconds]
+            else:
+                t1=timedelta(hours=int(prev[0]),minutes=int(prev[1]),seconds=int(prev[2]))
+                t2=timedelta(hours=int(hrs),minutes=int(minutes),seconds=int(seconds))
+                if((t2-t1).seconds>1):
+                    cumWattsSeconds+=(averageWatt)*((t2-t1).seconds-1)
+                    cumTime+=(t2-t1).seconds-1
+                prev=[hrs,minutes,seconds]
+            cumWattsSeconds+=int(watt)
+            cumTime+=1
+    f.close()
+    if(compress):
+        os.system("gzip "+fileName)
+    cumTimeHrs=cumTime/60/60
+    cumWattHrs=cumWattsSeconds/60/60
+    print("Estimate for date",fileName.split("_")[1].replace(".log",""))
+    print("Watt Hours for ",cumTimeHrs,"is ",cumWattHrs)
+    cumWatt24Hrs=(cumWattHrs/cumTimeHrs)*24
+    print("Watt Hours projected for 24hrs",cumWatt24Hrs)
+    return(cumTimeHrs,cumWattHrs,cumWatt24Hrs)
 plot=False
 log=True
 path="/var/log/apcupsmon"
 if(len(sys.argv)>=2):
+    if("--analyze" in " ".join(sys.argv) or "-a" in " ".join(sys.argv)):
+        newpath=None
+        try:
+            try:
+                newpath=sys.argv[sys.argv.index("--analyze")+1]
+            except:
+                newpath=sys.argv[sys.argv.index("-a")+1]
+            if("--" in newpath or "-p" in newpath or "-l" in newpath or "-f" in newpath or "-a" in newpath):
+                print("Invalid path!")
+                exit(-1)
+            path=newpath
+        except Exception as e:
+            print(e)
+            print("Path requires an argument!")
+            exit(-1)
+        #to be added function
+        fileToCumPower(newpath)
+        exit(0)
     if("--help" in " ".join(sys.argv) or "-h" in " ".join(sys.argv)):
         print("Example use cases")
         print(sys.argv[0],"-p # to plot and log to /var/log/apcupsmon")
@@ -53,7 +155,7 @@ if(len(sys.argv)>=2):
                 newpath=sys.argv[sys.argv.index("--path")+1]
             except:
                 newpath=sys.argv[sys.argv.index("-f")+1]
-            if("--" in newpath or "-p" in newpath or "-l" in newpath or "-f" in newpath):
+            if("--" in newpath or "-p" in newpath or "-l" in newpath or "-f" in newpath or "-a" in newpath):
                 print("Invalid path!")
                 exit(-1)
             path=newpath
